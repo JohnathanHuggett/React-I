41917e0f95515b0d15cd809093c1e62a45d2fd88
William <wtpwinberg@gmail.com>
2018-03-05 13:02:25 -0800

    Updated file name

---
+++ TodosInput.js
@@ -0,0 +1,5 @@
+import react, { component } from 'react';
+const BasicComponent = () => <div>Hello World!</div>;
+
+
+export default TodosInput;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
-test test test 
+//test test test 
 
 
 render(<App />, document.getElementById('root'));
